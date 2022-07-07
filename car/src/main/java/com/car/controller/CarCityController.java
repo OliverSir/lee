1ca242f41ecfc2cfecc155e5d7a196e3f584723a
LEE <lee@123>
2022-07-07 20:35:16 +0800

    11

--- car/src/main/java/com/car/controller/CarCityController.java
+++ car/src/main/java/com/car/controller/CarCityController.java
@@ -1,11 +1,15 @@
 package com.car.controller;
 
 import com.car.entity.CarCity;
-import com.car.service.CarCityService;
+import com.car.service.carPool.CarCityService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,11 +25,19 @@ import java.util.concurrent.atomic.AtomicInteger;
 @CrossOrigin
 @Slf4j
 public class CarCityController {
+    //11
     /**
      * 服务对象
      */
-    @Resource
-    private CarCityService carCityService;
+    @Resource(name = "carCityServiceImpl")
+    private CarCityService carCityServiceImpl;
+
+    @Autowired
+    HttpServletResponse httpServletResponse;
+
+    @Autowired
+    HttpSession httpSession;
+
 
     /**
      * 通过主键查询单条数据
@@ -35,8 +47,9 @@ public class CarCityController {
      */
     @GetMapping("selectOne")
     public CarCity selectOne(Long id) {
-        return this.carCityService.queryById(id);
+        return this.carCityServiceImpl.queryById(id);
     }
+
     /**
      * 通过省id查询批量数据
      *
@@ -49,7 +62,7 @@ public class CarCityController {
         atomicInteger.getAndIncrement();
         log.info("selectByPid-入参{}", id);
         Map<String, Object> result = new HashMap<>();
-        result.put("list", carCityService.selectByPid(id));
+        result.put("list", carCityServiceImpl.selectByPid(id));
         result.put("code", "00");
         log.info("selectByPid-反参{}", result);
         return result;
